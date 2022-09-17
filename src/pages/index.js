8b17ee0d80a8c5cae5ee51f530c7cfe186302e27
histov <histov@gmail.com>
2022-09-17 15:30:55 +0900

    수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/四書">
+            四書로 가기
           </Link>
         </div>
       </div>
