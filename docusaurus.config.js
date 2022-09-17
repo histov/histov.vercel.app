ab0b61d43df82e32846e0f15466da7d16aa3e87e
histov <histov@gmail.com>
2022-09-17 09:41:26 +0900

    new

---
+++ docusaurus.config.js
@@ -0,0 +1,224 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '한문원전정보',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ko',
+    locales: ['ko'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: '四書/四書',
+            position: 'left',
+            label: '四書',
+          },
+          {
+            type: 'doc',
+            docId: '集/集',
+            position: 'left',
+            label: '集',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: '四書',
+                to: '/docs/四書',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = {
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: 'docs',
+          breadcrumbs: true,
+          // Simple use-case: string editUrl
+          // editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          // Advanced use-case: functional editUrl
+          editUrl: ({versionDocsDirPath, docPath}) =>
+            `https://github.com/facebook/docusaurus/edit/main/website/${versionDocsDirPath}/${docPath}`,
+          editLocalizedFiles: false,
+          editCurrentVersion: false,
+          routeBasePath: 'docs',
+          include: ['**/*.md', '**/*.mdx'],
+          exclude: [
+            '**/_*.{js,jsx,ts,tsx,md,mdx}',
+            '**/_*/**',
+            '**/*.test.{js,jsx,ts,tsx}',
+            '**/__tests__/**',
+          ],
+          sidebarPath: 'sidebars.js',
+          async sidebarItemsGenerator({
+            defaultSidebarItemsGenerator,
+            numberPrefixParser,
+            item,
+            version,
+            docs,
+            isCategoryIndex,
+          }) {
+            // 제공된 데이터를 사용해 사용자 지정 사이드바 슬라이스 생성
+            return [
+              {type: 'doc', id: 'intro'},
+              {
+                type: 'category',
+                label: 'Tutorials',
+                items: [
+                  {type: 'doc', id: 'tutorial1'},
+                  {type: 'doc', id: 'tutorial2'},
+                ],
+              },
+            ];
+          },
+          numberPrefixParser(filename) {
+            // 잠재적인 숫자 접두사를 추출하는 여러분만의 논리를 구현합니다.
+            const numberPrefix = findNumberPrefix(filename);
+            // 접두사를 발견하면 정리된 파일명과 함께 반환합니다.
+            if (numberPrefix) {
+              return {
+                numberPrefix,
+                filename: filename.replace(prefix, ''),
+              };
+            }
+            // 숫자 접두사를 발견하지 못했습니다.
+            return {numberPrefix: undefined, filename};
+          },
+          docLayoutComponent: '@theme/DocPage',
+          docItemComponent: '@theme/DocItem',
+          remarkPlugins: [require('remark-math')],
+          rehypePlugins: [],
+          beforeDefaultRemarkPlugins: [],
+          beforeDefaultRehypePlugins: [],
+          showLastUpdateAuthor: false,
+          showLastUpdateTime: false,
+          disableVersioning: false,
+          includeCurrentVersion: true,
+          lastVersion: undefined,
+          versions: {
+            current: {
+              label: 'Android SDK v2.0.0 (WIP)',
+              path: 'android-2.0.0',
+              banner: 'none',
+            },
+            '1.0.0': {
+              label: 'Android SDK v1.0.0',
+              path: 'android-1.0.0',
+              banner: 'unmaintained',
+            },
+          },
+          onlyIncludeVersions: ['current', '1.0.0', '2.0.0'],
+        },
+      },
+    ],
+  ],
+};
