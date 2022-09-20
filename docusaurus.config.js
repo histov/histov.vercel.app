92d6170d506def1130a7bde5f0b9539958a156a1
histov <histov@gmail.com>
2022-09-20 16:09:10 +0900

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'G-4JTQT9RXMP',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
